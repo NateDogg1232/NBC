4a631d49baab670fdec64a418c0ec871e20a3d62
NateDogg1232 <nathanielbarragan@gmail.com>
2017-10-22 17:07:39 -0700

    I suck at git
    I had to recommit everything, sorry about that

---
+++ NBCAsm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NBCAsm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.GetUpperBound(0)!=0)
+            {
+                foreach (string arg in args)
+                {
+                    //Help argument
+                    if (arg == "-help" || arg == "-h")
+                    {
+                        Console.WriteLine("NBC Assembler Ver {0}", System.Reflection.Assembly.GetEntryAssembly().GetName().Version);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
