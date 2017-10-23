2974685078a01b916e6c730c2362e6061f73f20f
NateDogg1232 <nathanielbarragan@gmail.com>
2017-10-22 17:35:18 -0700

    Added some basics

--- NBCAsm/Program.cs
+++ NBCAsm/Program.cs
@@ -1,23 +1,41 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace NBCAsm
 {
     class Program
     {
+        static string fileName = "";
+
         static void Main(string[] args)
         {
-            if (args.GetUpperBound(0)!=0)
+            if (args.GetLength(0)!=0)
             {
+                Debug.WriteLine("There are args");
                 foreach (string arg in args)
                 {
                     //Help argument
                     if (arg == "-help" || arg == "-h")
                     {
-                        Console.WriteLine("NBC Assembler Ver {0}", System.Reflection.Assembly.GetEntryAssembly().GetName().Version);
+                        Debug.WriteLine("ArgumentFound: " + arg);
+                        Console.WriteLine("NBC Assembler Ver {0}", "b1.0" );
+                        Console.WriteLine("Archeticture: PC-.NETCore");
+                        Console.WriteLine();
+                        Console.WriteLine("Syntax: NBCAsm <file> [-h|-help]");
                         Console.ReadLine();
+                    }
+
+                    if (!arg.StartsWith("-"))
+                    {
+                        fileName = arg;
                     }
                 }
             }
+            else
+            {
+                Debug.WriteLine("There are no args");
+            }
+            Console.ReadLine();
         }
     }
 }
