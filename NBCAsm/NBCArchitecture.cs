42f80cbffc4758e019c545753e3cf7178b054fb9
NateDogg1232 <nathanielbarragan@gmail.com>
2017-11-18 21:23:08 -0800

    Added constructor to NBCArchitecture

--- NBCAsm/NBCArchitecture.cs
+++ NBCAsm/NBCArchitecture.cs
@@ -8,6 +8,33 @@ namespace NBCAssembler
     {
         public NBCArchitecture()
         {
+            //Set up standard/semi-standard ports
+            NBCPort tmpPort = new NBCPort
+            {
+                //Port 0: TTY Character out
+                PortID = 0,
+                InAllowed = false
+            };
+            Ports.Add(tmpPort);
+            tmpPort = new NBCPort
+            {
+                //Port 1: Character in
+                PortID = 1,
+                OutAllowed = false
+            };
+            Ports.Add(tmpPort);
+            tmpPort = new NBCPort
+            {
+                //Port 2: Get Current Architecture
+                PortID = 2
+            };
+            Ports.Add(tmpPort);
+            //Port 10h: Cursor Control
+            tmpPort = new NBCPort
+            {
+                PortID = 0x10
+            };
+            Ports.Add(tmpPort);
             
         }
 
